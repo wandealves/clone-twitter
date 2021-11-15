b77e5af74c6c2e018055c1cdb92e53e3e149d27f
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2021-11-15 12:51:13 -0300

    feat: criação tweet

--- src/components/Tweet/styles.ts
+++ src/components/Tweet/styles.ts
@@ -1,13 +1,11 @@
 import styled, { css } from 'styled-components'
 
-import { Chat, Rocketseat, Favorite } from 'styles/Icons'
+import { Chat, Rocketseat, Favorite } from '../../styles/Icons'
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-
   padding: 14px 16px;
-
   border-bottom: 1px solid var(--outline);
   max-width: 100%;
 `
@@ -15,18 +13,14 @@ export const Container = styled.div`
 export const Retweeted = styled.div`
   display: flex;
   align-items: center;
-
   font-size: 13px;
   color: var(--gray);
 `
-
 export const RocketseatIcon = styled(Rocketseat)`
   width: 16px;
   height: 16px;
-
   margin-left: 35px;
   margin-right: 9px;
-
   > path {
     fill: var(--gray);
   }
@@ -35,47 +29,38 @@ export const RocketseatIcon = styled(Rocketseat)`
 export const Body = styled.div`
   display: flex;
   margin-top: 3px;
-
   position: relative;
 `
 
 export const Avatar = styled.div`
   width: 49px;
   height: 49px;
+  border-radius: 50%;
   flex-shrink: 0;
   background: var(--gray);
-
   position: absolute;
   top: 0;
   left: 0;
 `
-
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-
   width: 100%;
   margin-top: 2px;
   padding-left: 59px;
 `
-
 export const Header = styled.div`
   display: flex;
   align-items: center;
-
   font-size: 15px;
-  white-space: nowrap; //para o texto não quebra linha
-
+  white-space: nowrap;
   > strong {
     margin-right: 5px;
   }
-
-  > ruby-span,
+  > span,
   time {
     color: var(--gray);
   }
-
-  //colocar os três pontinhos
   > strong,
   span {
     white-space: nowrap;
@@ -100,10 +85,8 @@ export const ImageContent = styled.div`
   margin-top: 12px;
   width: 100%;
   height: min(285px, max(175px, 41vw));
-
   background: var(--outline);
   border-radius: 14px;
-
   cursor: pointer;
   &:hover {
     opacity: 0.7;
@@ -116,15 +99,12 @@ export const Icons = styled.div`
   justify-content: space-between;
   flex-wrap: wrap;
   margin: 11px auto 0;
-  width: 100%;
-
+  width: 100%; /** Mobile */
   @media (min-width: 330px) {
     width: 63%;
   }
-
   > div {
     cursor: pointer;
-
     &:hover {
       opacity: 0.7;
     }
@@ -135,25 +115,21 @@ export const Status = styled.div`
   display: flex;
   align-items: center;
   font-size: 14px;
-
   > svg {
     margin-right: 5px;
   }
-
   &:nth-child(1) {
     &,
     > svg path {
       color: var(--gray);
     }
   }
-
   &:nth-child(2) {
     color: var(--retweet);
     > svg path {
       fill: var(--retweet);
     }
   }
-
   &:nth-child(3) {
     color: var(--like);
     > svg {
